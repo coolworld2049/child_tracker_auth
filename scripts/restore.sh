779c83a6012276065035cfb356be5ed6c12ea15b
coolworld2049 <coolworld2049@gmail.com>
2024-08-16 20:37:10 +0300

    add README.md

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo  "Restore dump"
-mariadb -u${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} < docker-entrypoint-initdb.d/dump.sql
-echo  "Apply migration"
-[ -f docker-entrypoint-initdb.d/migration.sql ] && mariadb -u${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} < docker-entrypoint-initdb.d/migration.sql
+mariadb -u${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} kidl < docker-entrypoint-initdb.d/dump.sql
+#echo  "Apply migration"
+#[ -f docker-entrypoint-initdb.d/migration.sql ] && mariadb -u${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} < docker-entrypoint-initdb.d/migration.sql
