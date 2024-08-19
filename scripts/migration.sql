f5140a142b00d4761286a27c4adb32e3f2e634dc
coolworld2049 <coolworld2049@gmail.com>
2024-08-19 16:57:44 +0300

    add col avatar_url

--- scripts/migration.sql
+++ scripts/migration.sql
@@ -1,4 +1,6 @@
-ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS phone varchar(100) NULL;
-ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS code INT NULL;
-ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS token varchar(255) NULL;
-ALTER TABLE kidl.members ADD UNIQUE (phone);
+--ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS phone varchar(100) NULL;
+--ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS code INT NULL;
+--ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS token varchar(255) NULL;
+--ALTER TABLE kidl.members ADD UNIQUE (phone);
+
+ALTER TABLE kidl.members ADD COLUMN IF NOT EXISTS avatar_url varchar(2000) NULL;
