f34a5b8434ee8fc061e4e46f3403c60447587beb
coolworld2049 <coolworld2049@gmail.com>
2024-10-03 17:15:34 +0300

    Сделать публичным апи метод для получения списка регионов

--- child_tracker_auth/web/api/utils/views.py
+++ child_tracker_auth/web/api/utils/views.py
@@ -1,17 +1,12 @@
 from typing import Literal
 
 from fastapi import APIRouter
-from fastapi.params import Depends
 
-from child_tracker_auth.security.oauth2 import get_current_member
 from child_tracker_auth.settings import settings
 
 router = APIRouter(
     prefix="/utils",
     tags=["Utils"],
-    dependencies=(
-        [Depends(get_current_member)] if settings.environment == "prod" else None
-    ),
 )
 
 
